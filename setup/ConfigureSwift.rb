a33e4fee5dfff2c001b27f7c21f73758ee4e02ea
Marcus Wu <marcus.wu0814@gmail.com>
2022-03-08 17:03:45 +0800

    Fix bug

--- setup/ConfigureSwift.rb
+++ setup/ConfigureSwift.rb
@@ -13,7 +13,33 @@ module Pod
 
     def perform
       keep_demo = "yes".to_sym
-      
+
+      framework = "none".to_sym
+      case framework
+        when :quick
+          configurator.add_pod_to_podfile "Quick', '~> 2.2.0"
+          configurator.add_pod_to_podfile "Nimble', '~> 8.0.7"
+          configurator.set_test_framework "quick", "swift", "swift"
+
+        when :none
+          configurator.set_test_framework "xctest", "swift", "swift"
+      end
+
+      snapshots = configurator.ask_with_answers("Would you like to do view based testing", ["Yes", "No"]).to_sym
+      case snapshots
+        when :yes
+          configurator.add_pod_to_podfile "FBSnapshotTestCase' , '~> 2.1.4"
+
+          if keep_demo == :no
+              puts " Putting demo application back in, you cannot do view tests without a host application."
+              keep_demo = :yes
+          end
+
+          if framework == :quick
+              configurator.add_pod_to_podfile "Nimble-Snapshots' , '~> 8.0.0"
+          end
+      end
+
       Pod::ProjectManipulator.new({
         :configurator => @configurator,
         :xcodeproj_path => "templates/swift/Example/PROJECT.xcodeproj",
