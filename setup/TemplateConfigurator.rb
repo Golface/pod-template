40d2a77f738149a92d712215b7e7e60578c2ce6e
Marcus Wu <marcus.wu0814@gmail.com>
2022-03-17 11:57:25 +0800

    Remove git command

--- setup/TemplateConfigurator.rb
+++ setup/TemplateConfigurator.rb
@@ -112,9 +112,6 @@ module Pod
       Dir.chdir("Example") do
         system "pod install"
       end
-
-      `git add Example/#{pod_name}.xcodeproj/project.pbxproj`
-      `git commit -m "Initial commit"`
     end
 
     def clean_template_files
